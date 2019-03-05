689f92b7b23f24017d1196d636a3c37404716ae4
fadi426 <fadiqadadi@hotmail.com>
2019-03-05 22:17:23 +0100

    Start on the Java Swing forms
    Base of the file structure has been made.
    A simple swing application has been made.
    MVC has been added.
    MAVEN has been added.
    dependencies have been adjusted.

---
+++ geometrydrawer/src/main/java/Launcher.java
@@ -0,0 +1,12 @@
+package pl.dmichalski.gui_swing;
+
+import controller.MainFrameController;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        MainFrameController controller = new MainFrameController();
+        controller.showMainFrameWindow();
+    }
+
+}
